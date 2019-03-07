692fb27259c211239bbe99ee924636373d3c9d8e
AljoschaMeyer <mail@aljoscha-meyer.de>
2019-03-07 05:59:35 +0100

    Render titles and descriptions (untested)

--- views/publication.js
+++ views/publication.js
@@ -5,9 +5,19 @@ module.exports = (state, emit) => {
 <div class="author">${state.author}</div>
 <time class="pubTime">Time: ${state.msg.timestamp}</time>
 ${
+  typeof state.msg.title === 'string' && state.msg.title.length > 0 ?
+  html`<h4 class="publicationTitle">${state.msg.title}</h4>` :
+  ''
+}
+${
   state.blob ?
   html`<img class="publicationImg" src="${URL.createObjectURL(new Blob(state.blob))}"></img>` :
   html`<div class="imgLoading">Image loading...</div>`
+}
+${
+  typeof state.msg.description === 'string' && state.msg.description.length > 0 ?
+  html`<div class="publicationDescription">${state.msg.description}</div>` :
+  ''
 }
 </div>`;
 };
