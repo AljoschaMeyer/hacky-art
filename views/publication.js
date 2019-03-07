322ef96efefb1847318b3eb6407ba694071f6987
AljoschaMeyer <mail@aljoscha-meyer.de>
2019-03-07 06:09:44 +0100

    Render human-friendly times

--- views/publication.js
+++ views/publication.js
@@ -1,9 +1,10 @@
 const html = require('choo/html');
+const human = require('human-time');
 
 module.exports = (state, emit) => {
   return html`<div class="publication">
 <div class="author">${state.author}</div>
-<time class="pubTime">Time: ${state.msg.timestamp}</time>
+<time class="pubTime">${human(new Date(state.msg.timestamp))}</time>
 ${
   typeof state.msg.title === 'string' && state.msg.title.length > 0 ?
   html`<h4 class="publicationTitle">${state.msg.title}</h4>` :
