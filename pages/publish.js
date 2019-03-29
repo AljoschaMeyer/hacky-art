8caa25042ebd30ed749e088e3642caa69c2cbe55
Zach! <webmaster@coolguy.website>
2019-03-30 00:35:12 +1300

    Adding image file to publish form displays image within form

--- pages/publish.js
+++ pages/publish.js
@@ -1,36 +1,57 @@
-const html = require('choo/html');
-const pull = require('pull-stream');
-const once = require('pull-stream/sources/once');
-const pullFileReader = require('pull-filereader');
+const html = require('choo/html')
+const pull = require('pull-stream')
+const once = require('pull-stream/sources/once')
+const pullFileReader = require('pull-filereader')
 
-const nav = require('../views/nav');
+const nav = require('../views/nav')
 
 module.exports = (state, emit) => {
   return html`<body>
   ${nav({ me: state.ssb.id })}
   ${
-    state.publishError ?
-    html`<div class="error">
-      ${
-        state.publishError.message === 'encoded message must not be larger than 8192 bytes' ?
-        html`The description, caption or title is too large, you'll need to cut some words.` :
-        html`An unexpected error occured.<code>${JSON.stringify(state.publishError)}</code>`
+  state.publishError
+    ? html`<div class="error">
+     ${
+    state.publishError.message === 'encoded message must not be larger than 8192 bytes' ?
+    html`The description, caption or title is too large, you'll need to cut some words.` :
+    html`An unexpected error occured.<code>${JSON.stringify(state.publishError)}</code>`
       }
-    </div>` :
-    html``
+    </div>`
+    : html``
   }
-  <form onsubmit="${onsubmit}">
-    <label for="imgInput">Select an image to publish</label>
-    <input type="file" name="imgInput" id="imgInput" accept="image/gif, image/jpeg, image/png, , image/apng" required>
+  <form onsubmit="${onsubmit}" id='publish-form'>
+    ${state.publishInput.blob
+      ? html`
+        <div>
+          <button onclick=${removeImage}>Remove Image</button>
+          <img class="publicationImg" src="${URL.createObjectURL(state.publishInput.blob)}" />
+        </div>`
+      : html`
+        <label for="imgInput">Select an image to publish</label>
+        <input type="file" onchange=${onChange} name="imgInput" id="imgInput" accept="image/gif, image/jpeg, image/png, , image/apng" required>
+      `
+    }
+    <label for="imgCaption">Caption</label>
+    <textarea name="imgCaption" id="imgCaption">${state.publishData.caption}</textarea>
     <label for="imgTitle">Title</label>
     <input type="text" name="imgTitle" id="imgTitle" value="${state.publishData.title}">
     <label for="imgDesc">Description</label>
     <textarea name="imgDesc" id="imgDesc">${state.publishData.description}</textarea>
-    <label for="imgCaption">Caption</label>
-    <textarea name="imgCaption" id="imgCaption">${state.publishData.caption}</textarea>
     <input type="submit" name="publish" value="Preview & Publish">
   </form>
 </body>`;
+
+  function onChange (event) {
+    event.preventDefault();
+    const imgInput = document.querySelector('#imgInput');
+    const imgFile = imgInput.files[0];
+    emit('preview image added', imgFile)
+  }
+
+  function removeImage () {
+    event.preventDefault()
+    emit('preview image removed')
+  }
 
   function onsubmit(event) {
     event.preventDefault();
--- stores/publish.js
+++ stores/publish.js
@@ -2,11 +2,23 @@ const client = require('ssb-client');
 
 module.exports = (state, emitter, app) => {
   state.publishData = {};
+  state.publishInput = {};
 
   emitter.on('DOMContentLoaded', () => {
     emitter.on('preview', data => {
       state.preview = data;
       emitter.emit('pushState', '/preview')
+    });
+
+    emitter.on('preview image added', data => {
+        state.publishInput.blob = data;
+        console.log({imgState: state.publishInput})
+        emitter.emit('pushState', '/publish')
+    });
+
+    emitter.on('preview image removed', data => {
+      state.publishInput.blob = null
+      emitter.emit('pushState', '/publish')
     });
 
     emitter.on('publishData', data => {
