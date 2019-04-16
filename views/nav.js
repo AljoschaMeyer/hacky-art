053f53ef6af1d6c03123e7c73b5901ec10c7a299
Jacob Karlsson <jacob.karlsson95@gmail.com>
2019-04-16 10:30:28 +0530

    Add back-button

--- views/nav.js
+++ views/nav.js
@@ -5,6 +5,7 @@ const { escapeKey } = require('../helpers/escape');
 module.exports = (state, emit) => {
   return html`<nav class="mainNav">
   <a href="/">Home</a>
+  <a href="javascript:history.back()">Go Back</a>
   ${
     state.me ?
     html`<a href="/user/${escapeKey(state.me)}">Me</a>` :
